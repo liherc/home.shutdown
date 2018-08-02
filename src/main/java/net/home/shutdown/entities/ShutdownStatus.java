327fb5e63d09935f89f06c616122aed1ecbe9c6e
Liher <lihercallejo@gmail.com>
2018-08-02 13:11:24 +0200

    Create README.md

--- src/main/java/net/home/shutdown/entities/ShutdownStatus.java
+++
@@ -1,5 +0,0 @@
-package net.home.shutdown.entities;
-
-public enum ShutdownStatus {
-    INPROGRESS, READY
-}
--- src/main/java/net/home/shutdown/managers/Shutdown.java
+++
@@ -1,9 +0,0 @@
-package net.home.shutdown.managers;
-
-import org.springframework.http.ResponseEntity;
-import java.io.IOException;
-
-public interface Shutdown {
-    ResponseEntity shutdown();
-    ResponseEntity getStatus();
-}
